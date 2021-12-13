ea683524b27e356649d7d427ce023f7b3b546ab0
Mishthisinghal <mishthi12singhal06@gmail.com>
2021-12-13 22:40:42 +0530

    added project links

--- js/port.js
+++
@@ -1,8 +0,0 @@
-let links=document.querySelectorAll(".scroll");
-links.forEach((item)=>{
-    item.addEventListener("click",()=>{
-        let el=document.getElementById(item.getAttribute("data-link"));
-        el.scrollIntoView({behavior:"smooth",block:"center"});
-    })
-}
-)
